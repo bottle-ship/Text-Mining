8170f0536ab5b08341068412c81f7533df192ae3
Byungseon Choi <garveira85@gmail.com>
2023-01-15 15:02:08 +0900

    Update WordTokenizer

--- text_mining/word_tokenizer.py
+++ text_mining/word_tokenizer.py
@@ -10,24 +10,19 @@ from nltk.tokenize import sent_tokenize
 
 class WordTokenizer(object):
 
-    def __init__(
-            self,
-            stopwords: t.Optional[t.Sequence[str]] = None,
-            passtags: t.Optional[t.Sequence[str]] = ("NNG", "NNP", "SL")
-    ):
+    def __init__(self, stopwords: t.Optional[t.Sequence[str]] = None):
         self.stopwords = list()
         if stopwords is not None:
             self.stopwords.extend(list(stopwords))
-        self.passtags = passtags
 
+        self._passtags = ("NNG", "NNP", "NNB", "SL")
         self._en_stopwords = list(set(nltk_stopwords.words("english")))  # noqa
         self._ko_tag = Komoran(model_path="EXP")
-        # self._ko_tag.set_user_dic()
 
     def tokenize(self, document: str, flatten: bool = True) -> t.List[t.List[t.Union[t.Tuple[str, str], str]]]:
         tokens = list()
         for sentence in sent_tokenize(document):
-            tokens.append(list())
+            tokens.append([list()])
             for token_tag in [token.split("/") for token in self._ko_tag.get_plain_text(sentence).split(" ")]:
                 tag = token_tag[-1]
                 token = "/".join(token_tag[:-1])
@@ -37,14 +32,23 @@ class WordTokenizer(object):
                     flag = False
                 if token in self.stopwords:
                     flag = False
-                if tag not in self.passtags:
+                if tag not in self._passtags:
                     flag = False
 
                 if flag:
-                    if self.passtags is None:
-                        tokens[-1].append((token, tag))
-                    else:
-                        tokens[-1].append(token)
+                    tokens[-1][-1].append(token)
+                else:
+                    if len(tokens[-1][-1]) > 0:
+                        tokens[-1].append(list())
+
+            if len(tokens[-1][-1]) == 0:
+                tokens[-1].pop()
+
+            for i in range(0, len(tokens[-1])):
+                if len(tokens[-1][i]) > 1:
+                    tokens[-1][i].append("-".join(tokens[-1][i]))
+
+            tokens[-1] = list(itertools.chain.from_iterable(tokens[-1]))
 
         if flatten:
             tokens = list(itertools.chain.from_iterable(tokens))
